a347d7c389710a16ff5a8ae0781de6ff45dac6ea
Alex Corbi <a.corbi@gmail.com>
2017-09-28 16:10:14 +0200

    ignoring test/ on brunch buil

--- brunch-config.js
+++ brunch-config.js
@@ -5,7 +5,11 @@ module.exports = {
       'leaflet-draw': ['dist/leaflet.draw.css'],
     },
   },
-
+  conventions: {
+    ignored: [
+      /^app\/test/
+    ],
+  },
   files: {
     javascripts: {
       joinTo: {
